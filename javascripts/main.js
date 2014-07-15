acd7f61f8180cae11a802865c3583c4d9234125a
wxypipi <wxy.pipi@gmail.com>
2014-07-15 15:22:38 +0800

    test1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,34 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+    $("#menuBtn").click(function(){
+        var mainBox = $("#mainBox");
+        // alert(mainBox.css("-webkit-transform"));
+        if(mainBox.css("-webkit-transform") != "matrix(1, 0, 0, 1, 0, 0)"){
+            mainBox.css("-webkit-transform","translate3d(0, 0, 0)");
+            // alert("yes");
+        }
+        else{
+            mainBox.css("-webkit-transform","translate3d(15em, 0, 0)");
+            // alert("no");
+        };
+    });
+
+    $("#contentsBtn").click(function(){
+        $("#menu").css("-webkit-transform","translate3d(0, 0, 0)");
+        $("#contentsBtn").css("box-shadow","0 -0.5em orange inset");
+        $("#searchBtn").css("box-shadow","0 0 orange inset");
+        $("#settingBtn").css("box-shadow","0 0 orange inset");
+    });
+    $("#searchBtn").click(function(){
+        $("#menu").css("-webkit-transform","translate3d(-15em, 0, 0)");
+        $("#searchBtn").css("box-shadow","0 -0.5em orange inset");
+        $("#contentsBtn").css("box-shadow","0 0 orange inset");
+        $("#settingBtn").css("box-shadow","0 0 orange inset");
+    });
+    $("#settingBtn").click(function(){
+        $("#menu").css("-webkit-transform","translate3d(-30em, 0, 0)");
+        $("#settingBtn").css("box-shadow","0 -0.5em orange inset");
+        $("#searchBtn").css("box-shadow","0 0 orange inset");
+        $("#contentsBtn").css("box-shadow","0 0 orange inset");
+    });
+});
