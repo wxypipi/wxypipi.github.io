f26e318fd79d84210b1486dcfc51d16e3c5e4ef5
wxypipi <wxy.pipi@gmail.com.com>
2014-07-26 15:31:02 +0800

    034

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,24 @@ $(document).ready(function(){
 //       e.preventDefault();
 //   });
 
+//uses document because document will be topmost level in bubbling
+$(document).on('touchmove',function(e){
+  e.preventDefault();
+});
+//uses body because jquery on events are called off of the element they are
+//added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart','.scrollable',function(e) {
+  if (e.currentTarget.scrollTop === 0) {
+    e.currentTarget.scrollTop = 1;
+  } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+    e.currentTarget.scrollTop -= 1;
+  }
+});
+//prevents preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove','.scrollable',function(e) {
+  e.stopPropagation();
+});
+
 
 alert("33");
 
@@ -337,7 +355,6 @@ $contentsItem.bind('touchend', function() {
     changeStep(($contentsItem.index(this))+1);
     selectStep(this);
     closeMenuBar();
-    
 });
 
 $previousBtn.bind('touchstart', function() { 
