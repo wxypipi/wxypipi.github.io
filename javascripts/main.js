fa5045a4f87ce00b3dc1bc4c10c59596f1c89ab0
wxypipi <wxy.pipi@gmail.com.com>
2014-08-01 15:27:29 +0800

    059

--- javascripts/main.js
+++ javascripts/main.js
@@ -244,7 +244,7 @@ function mainAnimationS() {
         };
         setTimeout(function(){
             changeImage();
-        },40)
+        },500)
     };
 };
 
@@ -260,7 +260,7 @@ function mainAnimationC() {
         };
         setTimeout(function(){
             changeImage();
-        },40)
+        },500)
     };
 };
 
