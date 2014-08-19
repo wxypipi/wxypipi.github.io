6275ab7b9fc456301894cbee71b562e4843eb91c
wxypipi <wxy.pipi@gmail.com.com>
2014-08-19 17:13:28 +0800

    136

--- javascripts/main.js
+++ javascripts/main.js
@@ -46,10 +46,10 @@ var mask = document.getElementById("mask"),
     imgL = document.getElementById("imgL"),
     imgR = document.getElementById("imgR"),
     imgM = document.getElementById("imgM"),
-    imgBox1 = document.getElementById("imgBox1"),
-    imgBox2 = document.getElementById("imgBox2"),
-    imgBox3 = document.getElementById("imgBox3"),
-    imgBox = [imgBox1,imgBox2,imgBox3],
+    imgBoxL = document.getElementById("imgBox1"),
+    imgBoxM = document.getElementById("imgBox2"),
+    imgBoxR = document.getElementById("imgBox3"),
+    imgBox = [imgBoxL,imgBoxM,imgBoxR],
     aniBox1 = document.getElementById("aniBox1"),
     aniBox2 = document.getElementById("aniBox2"),
     openMenuTouchArea = document.getElementById("openMenuTouchArea"),
@@ -90,9 +90,9 @@ function resize() {
             moveDis = (width+height)/2;
     };
 
-    imgBox[0].style.webkitTransform = "translate3d(-" + moveDis + "px,0,0)";
-    imgBox[1].style.webkitTransform = "translate3d(0,0,0)";
-    imgBox[2].style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
+    imgBoxL.style.webkitTransform = "translate3d(-" + moveDis + "px,0,0)";
+    imgBoxM.style.webkitTransform = "translate3d(0,0,0)";
+    imgBoxR.style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
     
     changeStepThreshold = Math.round(newWidth/10);
 
@@ -197,76 +197,75 @@ function slideChangeStep() {
     if (!touchEnd) {
         touchMoveDis = touchMovePos - touchStartPos;
         if (touchMoveDis > 0) {
-            imgBox1.style.webkitTransform = "translate3d(" + (- moveDis + touchMoveDis) + "px,0,0)";
-            imgBox2.style.webkitTransform = "translate3d(" + touchMoveDis + "px,0,0)";
+            imgBoxM.style.webkitTransform = "translate3d("+touchMoveDis+"px,0,0)";
+            imgBoxL.style.webkitTransform = "translate3d("+(touchMoveDis-moveDis)+"px,0,0)";
         } else {
-            imgBox2.style.webkitTransform = "translate3d(" + touchMoveDis + "px,0,0)";
-            imgBox3.style.webkitTransform = "translate3d(" + (moveDis + touchMoveDis) + "px,0,0)";
+            imgBoxM.style.webkitTransform = "translate3d("+touchMoveDis+"px,0,0)";
+            imgBoxR.style.webkitTransform = "translate3d("+(touchMoveDis+moveDis)+"px,0,0)";
         };
         window.requestAnimationFrame(slideChangeStep);
     } else {
+        frameIndex = 20;
+        factor = moveDis/Math.abs(touchMoveDis)-1;
         if (touchMoveDis == 0) {
-            //如果触摸点没有移动，执行这里的代码
             return;
-        };
-        animationing = true;
-        frameIndex = 12;
-        if (touchMoveDis > changeStepThreshold && currentStep != 1) {
-            factor = moveDis/touchMoveDis-1;
+        } else if (touchMoveDis > changeStepThreshold && currentStep != 1) {
             changeStepP();
         } else if (touchMoveDis < -changeStepThreshold && currentStep != maxStep) {
-            factor = -moveDis/touchMoveDis-1;
             changeStepN();
         } else if (touchMoveDis > 0) {
             notChangeStepP();
         } else {
             notChangeStepN();
         };
+        animationing = true;
     };
 };
 
 function changeStepN() {
-    touchEndMoveDis = touchMoveDis * (1 + factor * (1 - Math.pow(frameIndex / 12, 3)));
-    imgBox2.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
-    imgBox3.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis + moveDis) + "px,0,0)";
-    if (!separate) {
+    touchEndMoveDis = touchMoveDis * (1 + factor * (1 - Math.pow(frameIndex / 20, 3)));
+    imgBoxM.style.webkitTransform = "translate3d("+Math.round(touchEndMoveDis)+"px,0,0)";
+    imgBoxR.style.webkitTransform = "translate3d("+Math.round(touchEndMoveDis+moveDis)+"px,0,0)";
+    if (!separate && frameIndex < 13) {
         btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * frameIndex) + "px,0)";
     };
     frameIndex -= 1;
     if (frameIndex < 0) {
-        animationing = false;
-        if (!separate) {
-            separate = true;
-            btnFrameIndex = 0;
-            imgBox2.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";
-        };
-        currentStep += 1;
-        stepText.innerHTML = padding(currentStep);
-        imgBox1.style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
-        if (currentStep != maxStep) {
-            imgBox1.style.backgroundImage = "url(images/" + padding(currentStep+1) + "_S.png)";
-        };
-        mainAnimationImage.src = "images/" + padding(currentStep) + "_Animation.png";
-        var first = imgBox.shift();
-        imgBox.push(first);
-        imgBox1 = imgBox[0];
-        imgBox2 = imgBox[1];
-        imgBox3 = imgBox[2];
-        if (slideChangeStep2 && !touchEnd) {
-            touchStartPos = touchMovePos;
-            slideChangeStep();
-            slideChangeStep2 = false;
-        };
+        afterChangeStepN();
     } else {
         window.requestAnimationFrame(changeStepN);
     };
 };
 
-function changeStepP() {
-    touchEndMoveDis = touchMoveDis * (1 + factor * (1 - Math.pow(frameIndex / 12, 3)));
-    imgBox1.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis- moveDis) + "px,0,0)";
-    imgBox2.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
+function afterChangeStepN() {
+    animationing = false;
     if (!separate) {
+        separate = true;
+        btnFrameIndex = 0;
+        imgBoxM.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";
+    };
+    currentStep += 1;
+    stepText.innerHTML = padding(currentStep);
+    imgBoxL.style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
+    imgBoxL.style.backgroundImage = "url(images/" + padding(currentStep+1) + "_S.png)";
+    mainAnimationImage.src = "images/" + padding(currentStep) + "_Animation.png";
+    var first = imgBox.shift();
+    imgBox.push(first);
+    imgBoxL = imgBox[0];
+    imgBoxM = imgBox[1];
+    imgBoxR = imgBox[2];
+    if (slideChangeStep2 && !touchEnd) {
+        touchStartPos = touchMovePos;
+        slideChangeStep();
+        slideChangeStep2 = false;
+    };
+};
+
+function changeStepP() {
+    touchEndMoveDis = touchMoveDis * (1 + factor * (1 - Math.pow(frameIndex / 20, 3)));
+    imgBoxL.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis - moveDis) + "px,0,0)";
+    imgBoxM.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
+    if (!separate && frameIndex < 13) {
         btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * frameIndex) + "px,0)";
     };
     frameIndex -= 1;
@@ -275,16 +274,14 @@ function changeStepP() {
         if (!separate) {
             separate = true;//按钮状态归位
             btnFrameIndex = 0;//按钮动画帧数归零
-            imgBox2.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";//图片恢复为分离状态的图片
+            imgBoxM.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";//图片恢复为分离状态的图片
         };
         //改变标题栏的步骤
         currentStep -= 1;
         stepText.innerHTML = padding(currentStep);
         //将图片位置归位，并且替换图片
-        imgBox3.style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
-        if (currentStep != 1) {
-            imgBox3.style.backgroundImage = "url(images/" + padding(currentStep-1) + "_S.png)";
-        };
+        imgBoxR.style.webkitTransform = "translate3d(" + moveDis + "px,0,0)";
+        imgBoxR.style.backgroundImage = "url(images/" + padding(currentStep-1) + "_S.png)";
         //替换主动画图片
         mainAnimationImage.src = "images/" + padding(currentStep) + "_Animation.png";
         //将图片数组的排列顺序归位
@@ -292,9 +289,9 @@ function changeStepP() {
         imgBox.push(first);
         first = imgBox.shift();
         imgBox.push(first);
-        imgBox1 = imgBox[0];
-        imgBox2 = imgBox[1];
-        imgBox3 = imgBox[2];
+        imgBoxL = imgBox[0];
+        imgBoxM = imgBox[1];
+        imgBoxR = imgBox[2];
         if (slideChangeStep2 && !touchEnd) {
             touchStartPos = touchMovePos;
             slideChangeStep();
@@ -306,10 +303,10 @@ function changeStepP() {
 };
 
 function notChangeStepP() {
-    touchEndMoveDis = touchMoveDis * Math.pow(frameIndex / 12, 3);
+    touchEndMoveDis = touchMoveDis * Math.pow(frameIndex / 20, 3);
     frameIndex -= 1;
-    imgBox1.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis - moveDis) + "px,0,0)";
-    imgBox2.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
+    imgBoxL.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis - moveDis) + "px,0,0)";
+    imgBoxM.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
     if (frameIndex < 0) {
         animationing = false;
         if (slideChangeStep2 && !touchEnd) {
@@ -323,10 +320,10 @@ function notChangeStepP() {
 };
 
 function notChangeStepN() {
-    touchEndMoveDis = touchMoveDis * Math.pow(frameIndex / 12, 3);
+    touchEndMoveDis = touchMoveDis * Math.pow(frameIndex / 20, 3);
     frameIndex -= 1;
-    imgBox2.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
-    imgBox3.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis + moveDis) + "px,0,0)";
+    imgBoxM.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
+    imgBoxR.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis + moveDis) + "px,0,0)";
     if (frameIndex < 0) {
         animationing = false;
         if (slideChangeStep2 && !touchEnd) {
@@ -338,6 +335,27 @@ function notChangeStepN() {
         window.requestAnimationFrame(notChangeStepN);
     };
 };
+
+
+
+
+//==========================================按钮翻页===================================
+
+function changeStepN_Btn() {
+    touchEndMoveDis = -moveDis * (Math.cos(frameIndex / 20 * Math.PI)+1)/2;
+    imgBoxM.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
+    imgBoxR.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis + moveDis) + "px,0,0)";
+    if (!separate) {
+        btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * frameIndex) + "px,0)";
+    };
+    frameIndex -= 1;
+    if (frameIndex < 0) {
+        afterChangeStepN();
+    } else {
+        window.requestAnimationFrame(changeStepN_Btn);
+    };
+};
+
 
 
 
@@ -389,7 +407,7 @@ function slideCloseMenu() {
         if (touchMoveDis >= 0) {
             //关闭菜单栏
             animationing = true;
-            frameIndex = 40;
+            frameIndex = 36;
             closeMenu_Btn();
             return;
         };
@@ -444,7 +462,7 @@ function closeMenu() {
 //====================================按钮开启菜单栏==========================================
 
 function openMenu_Btn() {
-    touchEndMoveDis = 240 * (1-Math.pow(frameIndex / 40, 8));
+    touchEndMoveDis = 240 * (1-Math.pow(frameIndex / 36, 8));
     frameIndex -= 1;
     mainBox.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
     menuBox.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis/2-120) + "px,0,0)";
@@ -458,7 +476,7 @@ function openMenu_Btn() {
 };
 
 function closeMenu_Btn() {
-    touchEndMoveDis = 240 * Math.pow(frameIndex / 40, 8);
+    touchEndMoveDis = 240 * Math.pow(frameIndex / 36, 8);
     frameIndex -= 1;
     mainBox.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis) + "px,0,0)";
     menuBox.style.webkitTransform = "translate3d(" + Math.round(touchEndMoveDis/2-120) + "px,0,0)";
@@ -487,19 +505,11 @@ var cxt = mainAnimation.getContext("2d"),
 mainAnimationImage.src = "images/001_Animation.png";
 
 function mainAnimationC() {
-    if (btnFrameIndex < 12) {
-        btnFrameIndex += 1;
-        btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * btnFrameIndex) + "px,0)";
-        if (tickCount > ticksPerFrame) {
-            tickCount = 0;
-            frameIndex += 1;
-            cxt.drawImage(mainAnimationImage,
-            0,400 * frameIndex, 400, 400, 0, 0, 400, 400);
-        } else {
-            tickCount += 1;
+    if (frameIndex < 19) {
+        if (btnFrameIndex < 12) {
+            btnFrameIndex += 1;
+            btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * btnFrameIndex) + "px,0)";
         };
-        window.requestAnimationFrame(mainAnimationC);
-    } else if (frameIndex < 19) {
         if (tickCount > ticksPerFrame) {
             tickCount = 0;
             frameIndex += 1;
@@ -510,7 +520,7 @@ function mainAnimationC() {
         };
         window.requestAnimationFrame(mainAnimationC);
     } else if (frameIndex == 19) {
-        imgBox2.style.backgroundImage = "url(images/" + padding(currentStep) + "_C.png)";
+        imgBoxM.style.backgroundImage = "url(images/" + padding(currentStep) + "_C.png)";
         frameIndex += 1;
         window.requestAnimationFrame(mainAnimationC);
     } else if (frameIndex < 24) {
@@ -524,19 +534,11 @@ function mainAnimationC() {
 };
 
 function mainAnimationS() {
-    if (btnFrameIndex > 0) {
-        btnFrameIndex -= 1;
-        btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * btnFrameIndex) + "px,0)";
-        if (tickCount > ticksPerFrame) {
-            tickCount = 0;
-            frameIndex -= 1;
-            cxt.drawImage(mainAnimationImage,
-            0,400 * frameIndex,400,400,0,0, 400, 400);
-        } else {
-            tickCount += 1;
+    if (frameIndex > 0) {
+        if (btnFrameIndex > 0) {
+            btnFrameIndex -= 1;
+            btnAnimation.style.webkitTransform = "translate3d(0,-" + (112 * btnFrameIndex) + "px,0)";
         };
-        window.requestAnimationFrame(mainAnimationS);
-    } else if (frameIndex > 0) {
         if (tickCount > ticksPerFrame) {
             tickCount = 0;
             frameIndex -= 1;
@@ -547,7 +549,7 @@ function mainAnimationS() {
         };
         window.requestAnimationFrame(mainAnimationS);
     } else if (frameIndex == 0) {
-        imgBox2.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";
+        imgBoxM.style.backgroundImage = "url(images/" + padding(currentStep) + "_S.png)";
         frameIndex -= 1;
         window.requestAnimationFrame(mainAnimationS);
     } else if (frameIndex > -5) {
@@ -586,7 +588,7 @@ menuBtn.addEventListener('touchend', function(){
     // selectStep($contentsItem[currentStep-1]);
     if (!animationing) {
         animationing = true;
-        frameIndex = 40;
+        frameIndex = 36;
         if (menuOpened) {
             closeMenu_Btn();
         } else {
@@ -641,7 +643,8 @@ previousGlow.addEventListener('webkitAnimationEnd', function(){
 
 $nextBtn.bind('touchstart', function() { 
     nextGlow.style.webkitAnimation = "glow 0.3s linear";
-    changeStepN();
+    frameIndex = 20;
+    changeStepN_Btn();
 });
 
 nextGlow.addEventListener('webkitAnimationEnd', function(){
@@ -654,9 +657,11 @@ btnAnimation.addEventListener('touchend', function(){
         animationing = true;
         if (separate) {
             frameIndex = 0;
+            btnFrameIndex = 0;
             mainAnimationC();
         }else {
             frameIndex = 19;
+            btnFrameIndex = 12;
             mainAnimationS();
         };
     };
